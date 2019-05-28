e70dc87221dcec927ee08e685051824189da9658
Austin T <austin.taranto@gmail.com>
2019-05-28 15:52:18 -0400

    function changes and edits

--- javascript.js
+++ javascript.js
@@ -67,25 +67,25 @@ $.ajax({
 
 }
 
-// Function for displaying movie data
+// Function for displaying gifs
 function renderButtons() {
 
-  // Deleting the movies prior to adding new movies
+  // Deleting the gifs prior to adding new gifs
   // (this is necessary otherwise you will have repeat buttons)
   $("#buttons-view").empty();
 
-  // Looping through the array of movies
-  for (var i = 0; i < movies.length; i++) {
+  // Looping through the array of gifs
+  for (var i = 0; i < things.length; i++) {
 
-    // Then dynamicaly generating buttons for each movie in the array
+    // Then dynamicaly generating buttons for each gifs in the array
     // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
     var a = $("<button>");
-    // Adding a class of movie-btn to our button
-    a.addClass("movie-btn");
+    // Adding a class of star-wars-btn to our button
+    a.addClass("star-wars-btn");
     // Adding a data-attribute
-    a.attr("data-name", movies[i]);
+    a.attr("id", things[i]);
     // Providing the initial button text
-    a.text(movies[i]);
+    a.text(things[i]);
     // Adding the button to the buttons-view div
     $("#buttons-view").append(a);
   }
