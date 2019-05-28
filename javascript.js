bcaa88a21ec275abcd8814e01281c3c28e2b6d2b
Austin T <austin.taranto@gmail.com>
2019-05-27 22:14:45 -0400

    trying to javascript and jquery from class activity

--- javascript.js
+++ javascript.js
@@ -6,5 +6,25 @@ function displayStarWarsInfo(){
     var SW = $(this).attr("id");
     var queryURL = "https://www.omdbapi.com/?t=" + 1XFs0hWuTTHZjLE2c02BWlTFYr80TUbv + "&apikey=trilogy";
 
+//creating ajax
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+    //div to hold sw thing
+    var SwThing = $("<div class='sw'>");
+
+    //storing the data 
+    var data = response.Data;
+
+    // //creating an element to have things displayed
+    // var funFact = $("")
+
+    //displaying SW things
+    SwThing.append();
+
+    //stroing things
     
+    
+})
 }
