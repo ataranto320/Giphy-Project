58f3712ebcf770b9f15d5260eaa2cb4f147bba9e
Austin T <austin.taranto@gmail.com>
2019-05-27 21:42:29 -0400

    added api key in placeholder

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
 // array of star wars stuff
 var things = ["Ships", "Jedi", "Sith", "Bounty Hunters"];
 
+//display info function to show content 
+function displayStarWarsInfo(){
+    var SW = $(this).attr("id");
+    var queryURL = "https://www.omdbapi.com/?t=" + 1XFs0hWuTTHZjLE2c02BWlTFYr80TUbv + "&apikey=trilogy";
+
+    
+}
