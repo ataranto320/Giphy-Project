7667a0f0e377f622c63a7207464a039de676f9ae
Austin T <austin.taranto@gmail.com>
2019-05-28 22:16:15 -0400

    added background, color, and margins

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ var things = ["Ships", "Jedi", "Sith", "Bounty Hunters"];
 //display info function to show content 
 function displayStarWarsInfo(){
     var SW = $(this).attr("id");
-    var queryURL = "https://www.omdbapi.com/?t=" + 1XFs0hWuTTHZjLE2c02BWlTFYr80TUbv + "&apikey=trilogy";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + 1XFs0hWuTTHZjLE2c02BWlTFYr80TUbv + "&apikey=trilogy";
 
 //creating ajax
 $.ajax({
